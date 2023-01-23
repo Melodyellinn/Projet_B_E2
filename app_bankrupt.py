5b5acc68beff61d7ad2aaab1fd5272c373fed072
Melody Migliaccio <welodya@gmail.com>
2023-01-23 13:50:10 +0100

    change app1

--- app_bankrupt.py
+++ app_bankrupt.py
@@ -1,6 +1,7 @@
 
 import pickle
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 import streamlit as st
 import pandas as pd
 import imblearn
