27ce8ad03df71055db9869a1a588d85b33e4eb2b
Melody Migliaccio <welodya@gmail.com>
2023-01-23 15:32:03 +0100

    try to change app1

--- app_bankrupt.py
+++ app_bankrupt.py
@@ -8,7 +8,8 @@ import numpy as np
 import imblearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_curve,auc,\
+    confusion_matrix, classification_report
 import pyarrow as pa
 
 ## Import pickle & Data ##
@@ -32,27 +33,27 @@ def predict(prediction):
 #     #     y_pred = 'Non Faillite'
 #     # elif y_pred == 1:
 #     #     y_pred = 'Attention risque de Faillite'
-
-#conf_mat = confusion_matrix(y_test, predict)
-#sns.heatmap(conf_mat, annot=True, fmt="d", cmap="Reds")
-#plt.xlabel("Predicted")
-#plt.ylabel("Actual")
-
+y_pred = model.predict(X_test)
 model_prediction = pd.DataFrame()
 model_prediction['Real_Value'] = y_test
 model_prediction['Prediction_Value'] = predict
 
 ### APP FRONT ###
 row_1_margin_1, row_1_col_1, row_2_col_2, row_1_margin_2 = st.columns((.2, 4.5, 2.5,.2))
-with row_2_col_2:
-    st.title('Bankrupt or not Bankrupt ?')
 with row_1_col_1:
+    st.title('Bankrupt or not Bankrupt ?')
+with row_2_col_2:
     if st.button('Predict'):
         pred = predict
         st.success(f'The predictions were successful')
         st.dataframe(model_prediction)
 
-# with row_2_col_2:
+with row_1_col_1:
 #     st.title('Confusion Matrix')
-#     st.write('test')
-#     st.plotly_chart(conf_mat, use_container_width=False)
+        conf_mat = confusion_matrix(y_test, y_pred)
+        sns.heatmap(conf_mat, annot=True, fmt="d", cmap="Reds")
+        plt.xlabel("Predicted")
+        plt.ylabel("Actual")
+        st.write('test')
+        if st.button('Report'):
+            st.plotly_chart(conf_mat, use_container_width=False)
