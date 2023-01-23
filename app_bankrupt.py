8423cbd319252c95d33216e1eaa4c95d551e4489
Melody Migliaccio <welodya@gmail.com>
2023-01-23 14:26:56 +0100

    change app8

--- app_bankrupt.py
+++ app_bankrupt.py
@@ -27,16 +27,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
 ### define predict def ###   
 def predict(prediction):
     y_pred = model.predict(X_test)
-    if y_pred == 0:
-        y_pred = 'Non Faillite'
-    elif y_pred == 1:
-        y_pred = 'Attention risque de Faillite'
+    # if y_pred == 0:
+    #     y_pred = 'Non Faillite'
+    # elif y_pred == 1:
+    #     y_pred = 'Attention risque de Faillite'
 
     return prediction
 
 model_prediction = pd.DataFrame()
 model_prediction['Real_Value'] = y_test
-model_prediction['Prediction_Value'] = predict()
+model_prediction['Prediction_Value'] = predict
 
 #conf_mat = confusion_matrix(y_test, predict)
 #sns.heatmap(conf_mat, annot=True, fmt="d", cmap="Reds")
@@ -48,7 +48,7 @@ row_1_margin_1, row_1_col_1, row_2_col_2, row_1_margin_2 = st.columns((.2, 4.5, 
 with row_1_col_1:
     st.title('Bankrupt or not Bankrupt ?')
     if st.button('Predict'):
-        bankrupt = predict()
+        bankrupt = predict
         st.success(f'The predictions were successful')
         st.dataframe(model_prediction)
 
