afa12d4aca69343c0d713e320264120817b312d7
Melody Migliaccio <welodya@gmail.com>
2023-01-23 14:43:32 +0100

    change app11

--- app_bankrupt.py
+++ app_bankrupt.py
@@ -14,8 +14,6 @@ data = pd.read_csv("data/data.csv")
 
 with open('True_model_RandomForest.pkl', 'rb') as file:
     model = pickle.load(file)
-    
-model_prediction = pd.DataFrame()
 
 # Define the target and split data for train_test
 y = data['Bankrupt?']
@@ -27,15 +25,20 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
 # model_train = model.fit(X_train, y_train)
 
 ### define predict def ###   
-def predict(prediction):
-    y_pred = model.predict(X_test)
-    # if y_pred == 0:
-    #     y_pred = 'Non Faillite'
-    # elif y_pred == 1:
-    #     y_pred = 'Attention risque de Faillite'
-    model_prediction['Real_Value'] = y_test
-    model_prediction['Prediction_Value'] = y_pred
-    return prediction
+# def predict(prediction):
+#     y_pred = model.predict(X_test)
+#     # if y_pred == 0:
+#     #     y_pred = 'Non Faillite'
+#     # elif y_pred == 1:
+#     #     y_pred = 'Attention risque de Faillite'
+
+#     return prediction
+
+
+y_pred = model.predict(X_test)
+model_prediction = pd.DataFrame()
+model_prediction['Real_Value'] = y_test
+model_prediction['Prediction_Value'] = y_pred
 
 #conf_mat = confusion_matrix(y_test, predict)
 #sns.heatmap(conf_mat, annot=True, fmt="d", cmap="Reds")
@@ -47,7 +50,6 @@ row_1_margin_1, row_1_col_1, row_2_col_2, row_1_margin_2 = st.columns((.2, 4.5, 
 with row_1_col_1:
     st.title('Bankrupt or not Bankrupt ?')
     if st.button('Predict'):
-        bankrupt = predict
         st.success(f'The predictions were successful')
         st.dataframe(model_prediction)
 
