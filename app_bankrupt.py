992fdbd089eac55126bcd6cb29b43a618b28df4f
Melody Migliaccio <welodya@gmail.com>
2023-01-23 15:52:44 +0100

    try to change app7

--- app_bankrupt.py
+++ app_bankrupt.py
@@ -6,8 +6,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import imblearn
-from plotly.subplots import make_subplots
-import plotly.express
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
@@ -39,23 +37,20 @@ model_prediction['Real_Value'] = y_test
 model_prediction['Prediction_Value'] = predict(X_test)
 
 ### APP FRONT ###
-row_1_margin_1, row_1_col_1, row_2_col_2, row_1_margin_2 = st.columns((.2, 2.5, 4.5,.2))
+row_1_margin_1, row_1_col_1, row_2_col_2, row_1_margin_2 = st.columns((.2, 4.5, 2.5,.2))
 with row_1_col_1:
     st.title('Bankrupt or not Bankrupt ?')
-with row_2_col_2:
     if st.button('Predict'):
         pred = predict
         st.success(f'The predictions were successful')
         st.dataframe(model_prediction)
-        
-row_1_margin_1, row_3_col_3, row_1_margin_2 = st.columns((.2, 4.5,.2))
-with row_3_col_3:
-#     st.title('Confusion Matrix')
+
+with row_2_col_2:
+    if st.button('Report'):
         conf_mat = confusion_matrix(y_test, predict(X_test))
         sns.heatmap(conf_mat, annot=True, fmt="d", cmap="Reds")
         plt.title("Confusion matrix")
         plt.xlabel("Predicted")
         plt.ylabel("Actual")
         st.write('test')
-        if st.button('Report'):
-            st.plotly_chart(conf_mat, use_container_width=False)
+        plt.show()
